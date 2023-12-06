1a1beadf5fe0245d66893055c3c3b009d8f92afa
1norey <rb67875@ubt-uni.net>
2023-12-06 23:51:11 +0100

    Html Login validation update

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
-// // document.addEventListener("DOMContentLoaded",
-//     function (event) {
-//         const BtnSubmit = document.getElementById('submit-btn');
-//         const validate = (event) => {
-//             event.preventDefault();
-//             const username = document.getElementById('userid');
-//             const password = document.getElementById('pass');
-//             if (username.value === "") {
-//                 alert("Enter your username");
-//                 username.focus();
-//                 return false;
-//             }
-//             if (password.value === "") {
-//                 alert("Enter your password");
-//                 password.focus();
-//                 return false;
-//             }
-//             }
-//             if (!usernameValid(username.value)) {
-//                 alert("Please enter your username");
-//                 username.focus();
-//                 return false;
-//             }
-//             return true; 
-//             const usernameValid = (username) => {
-//                 const usernameRegex = /^([A-Za-z0-9_-.])\s $ /;
-// return usernameRegex.test(username.toLowerCase());
-//                 }
-// BtnSubmit.addEventListener('click', validate);
-               
-//                 if (!passwordValid(password.value)) {
-//                     alert("Please enter your username");
-//                     password.focus();
-//                     return false;
-//                 }
-//                 return true; 
-//                 const passwordValid = (password) => {
-//                     const passwordRegex = /^([A-Za-z0-9_-.!?])\s $ /;
-//     return passwordRegex.test(password.toLowerCase());
-//                     }
-//     BtnSubmit.addEventListener('click', validate);}
-//                 // });
-    
-    
+function validateForm() {
+    validateForm.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username.value === "" ) {
+        alert("Please enter a valid username.");
+        username.focus();
+        return false;
+    }
+    if (password.value === "" ) {
+        alert("Please enter a valid password.");
+        password.focus();
+        return false;
+    }
+    let usernameRegex = /^[a-zA-Z0-9._-]\s+$/;
+    if (!usernameRegex.test(username.toLowerCase())) {
+        alert("Username can only contain letters (a-z, A-Z) and numbers (0-9).");
+        return false;
+    }
+    let passwordRegex = /^[a-zA-Z0-9.-_!?]\s+$/;
+    if (!passwordRegex.test(password)) {
+        alert("Password can only contain letters (a-z, A-Z) and numbers (0-9).");
+        return false;
+    }
+
+    return true; 
+}
