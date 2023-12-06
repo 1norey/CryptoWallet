12ae7141b42c2b0fb49491abf44b59a1738cf5c6
1norey <rb67875@ubt-uni.net>
2023-12-06 03:57:32 +0100

    Log in

---
+++ script.js
@@ -0,0 +1,44 @@
+// // document.addEventListener("DOMContentLoaded",
+//     function (event) {
+//         const BtnSubmit = document.getElementById('submit-btn');
+//         const validate = (event) => {
+//             event.preventDefault();
+//             const username = document.getElementById('userid');
+//             const password = document.getElementById('pass');
+//             if (username.value === "") {
+//                 alert("Enter your username");
+//                 username.focus();
+//                 return false;
+//             }
+//             if (password.value === "") {
+//                 alert("Enter your password");
+//                 password.focus();
+//                 return false;
+//             }
+//             }
+//             if (!usernameValid(username.value)) {
+//                 alert("Please enter your username");
+//                 username.focus();
+//                 return false;
+//             }
+//             return true; 
+//             const usernameValid = (username) => {
+//                 const usernameRegex = /^([A-Za-z0-9_-.])\s $ /;
+// return usernameRegex.test(username.toLowerCase());
+//                 }
+// BtnSubmit.addEventListener('click', validate);
+               
+//                 if (!passwordValid(password.value)) {
+//                     alert("Please enter your username");
+//                     password.focus();
+//                     return false;
+//                 }
+//                 return true; 
+//                 const passwordValid = (password) => {
+//                     const passwordRegex = /^([A-Za-z0-9_-.!?])\s $ /;
+//     return passwordRegex.test(password.toLowerCase());
+//                     }
+//     BtnSubmit.addEventListener('click', validate);}
+//                 // });
+    
+    
