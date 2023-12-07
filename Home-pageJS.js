46812fe75f9c4e319f5dde80644675764d411abc
1norey <rb67875@ubt-uni.net>
2023-12-07 23:23:26 +0100

    Chnages HomePage

---
+++ Home-pageJS.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+ let btc=document.getElementById("bitcoin");
+ let eth=document.getElementById("ethereum");
+ let doge=document.getElementById("dogecoin");
+
+
+ let settings={
+     "async":true,
+     "crossDomain":true,
+     "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+     "method":"GET",
+     "headers":{}
+ }
+ $.ajax(settings).done(function(response){
+     // console.log(response);
+      btc.innerHTML = response.bitcoin.usd; 
+      eth.innerHTML = response.ethereum.usd;
+      doge.innerHTML = response.dogecoin.usd;
+ }) ;
+});
